8db2f80234f82879f616c02be93349072b18f2de
FabPch <fabien191@hotmail.com>
2023-02-19 11:17:40 +0100

    list favorites and remove

--- api/dao/favorites.py
+++ api/dao/favorites.py
@@ -24,7 +24,27 @@ class FavoriteDAO:
     def all(self, user_id, sort = 'title', order = 'ASC', limit = 6, skip = 0):
         # TODO: Open a new session
         # TODO: Retrieve a list of movies favorited by the user
-        return popular
+        def list_favorite(tx, user_id, limit, order, sort, skip):
+            result = tx.run("""
+                MATCH (u:User {{userId: $userId}})-[r:HAS_FAVORITE]->(m:Movie)
+                RETURN m {{
+                    .*
+                    , favorite: true
+                }} AS favorites
+                ORDER BY m.`{0}` {1}
+                SKIP $skip
+                LIMIT $limit
+            """.format(sort, order), userId=user_id, limit=limit, skip=skip)
+
+            return result.value('favorites')
+        
+        with self.driver.session() as session:
+            return session.execute_read(list_favorite
+                                        , user_id=user_id
+                                        , limit=limit
+                                        , order=order
+                                        , sort=sort
+                                        , skip=skip)
     # end::all[]
 
 
@@ -79,6 +99,23 @@ class FavoriteDAO:
         # TODO: Define a transaction function to delete the HAS_FAVORITE relationship within a Write Transaction
         # TODO: Execute the transaction function within a Write Transaction
         # TODO: Return movie details and `favorite` property
+        def add_to_favorite(tx, user_id, movie_id):
+            result = tx.run("""
+                MATCH (u:User {userId: $userId})-[r:HAS_FAVORITE]->(m:Movie {tmdbId: $movieId})
+                DELETE r
+                RETURN m {
+                    .*
+                    , favorite: false
+                } AS movie
+            """, userId=user_id, movieId=movie_id).single()
+
+            if result is None:
+                raise NotFoundException()
+            
+            return result.get('movie')
+        
+        with self.driver.session() as session:
+            return session.execute_write(add_to_favorite, user_id=user_id, movie_id=movie_id)
 
         return {
             **goodfellas,
